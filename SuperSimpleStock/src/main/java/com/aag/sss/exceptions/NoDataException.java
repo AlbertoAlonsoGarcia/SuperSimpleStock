6ac3f0fed83a2806ac0f02cbaac783a114174fa7
AlbertoAlonsoGarcia <alber_alonso@hotmail.com>
2016-03-23 12:13:47 +0100

    Exception created for solving the No Data issue
    An exception has been created to recognize especial situations when the system is not failing but it soen's have enough data to

---
+++ SuperSimpleStock/src/main/java/com/aag/sss/exceptions/NoDataException.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.aag.sss.exceptions;
+
+/**
+ * @author Alberto Alonso
+ *
+ */
+@SuppressWarnings("serial")
+public class NoDataException extends Exception {
+	
+	public NoDataException(String message)
+	{
+		super(message);
+	}
+
+}
